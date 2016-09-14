c406cefb4941018820fb92d98bffd4c88f6d20c2
Cascadian <alex.zwingli@outlook.com>
2016-09-14 08:34:45 -0600

    Removed unessecary folders

---
+++ Olympic_Soccer_Team/Olympic_Soccer_Team/Team.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Team //PARENT CLASS
+    {
+        //DECLARE VARIABLES
+        public string sName { get; set; }
+        public int iWins { get; set; }
+        public int iLosses { get; set; }
+
+        //CONSTRUCTOR(S)
+        public Team() { } //DEFAULT CONSTRUCTOR
+
+        public Team(string sName)
+        {
+            this.sName = sName;
+        }
+    }
+}
