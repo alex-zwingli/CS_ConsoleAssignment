dd731c89845b9558b5d3fc9eacec84cceb93f7dc
Cascadian <alex.zwingli@outlook.com>
2016-09-14 09:55:41 -0600

    Added empty game class

---
+++ Olympic_Soccer_Team/Olympic_Soccer_Team/game.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Olympic_Soccer_Team
+{
+    class game
+    {
+    }
+}
